d9e1107bec785c6ceea62840728ed6d36792ad33
MatteoBira <mattebiroo@gmail.com>
2025-03-26 17:30:29 +0100

    blah blah blah bleh bleh bleh

--- collezionismo.cpp
+++ collezionismo.cpp
@@ -1,72 +1,50 @@
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <numeric>
-#include <set>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int pop(vector<int>& vec){
-    int a = vec[vec.size()];
-    vec.erase(vec.end());
+// 1-sort the statue vector
+// 2-while all k scaffali fill them with the statua with max D(defined by min D between next and previous statue)
+// 3-distribute each remaining statue in the scaffale with hyp least D
+
+int pop(vector<int>& vec) {
+    int a = vec.back();
+    vec.pop_back();
     return a;
 }
 
-int pop_front(vector<int>& vec){
-    int a = vec[0];
-    vec.erase(vec.begin);
+int pop_front(vector<int>& vec) {
+    int a = vec.front(); // Get first element
+    vec.erase(vec.begin()); // Remove first element
     return a;
 }
 
-vector<vector<int>> bordering(vector<int>& c, int k){
-    vector<vector<int>> res;
-    vector<int> mom = {};
-    if(k%2 == 0){
-        for(int i=0; i<k; i++){
-            mom.push_back(c.pop());
-            res.push_back(mom);
-            mom.pop();
-
-            mom.push_back(c.pop_front());
-            res.push_back(mom);
-            mom.pop();
-        }
-    }else{
-        for(int i=0; i<k-1; i++){
-            mom.push_back(c.pop());
-            res.push_back(mom);
-            mom.pop();
-
-            mom.push_back(c.pop_front());
-            res.push_back(mom);
-            mom.pop();
-        }
-        mom.push_back(c.pop_front());
-        res.push_back(mom);
-    }
-    return res;
-}
-
-int distribute(vector<int> c, vector<vector<int>> scaf){
-    int curRes = 0;
-    for(int i=0; i<c.size(); i++){
-        int min = abs(scaf[0][0] - c[i]);
-        int pos = 0;
-        for(int j=0; j<scaf.size(); j++){
-            int cur_min = abs(scaf[j][0] - c[i]);
-            if(cur_min < min){
-                pos = j; 
-                min = cur_min;
-            }
-        }
-        scaf[j].push_back(c[i]);
-        curRes -= abs(scaf[j][0] - c[]) //PORCO DIO
+void distribute_first_layer(vector<vector<int>>& scaffali, vector<int>& vec, int k){
+    int idx;
+    vector<int> vec_copy = vec;
+    for(int i=0; i<k; i++){
+        idx = find_nmax_d(vec_copy, i);
+        scaffali.push_back({vec_copy[i],vec_copy[i]}); // NOTE: idem a sotto 
+        vec.erase(vec[i]); // NOTE: non so se cosi sia corretto
     }
 }
+
+void distribute_remaining_statues(vector<vector<int>>& scaffali, vector<int>& vec, int k){
+    for(int i=0; i<vec.size(), i++){
+        add_best_scafale(scaffali, vec[i], k);
+    }
+
+
+void add_best_scaffale(vector<vector<int>>& scaffali, vector<int>& vec, int k){
+    
+    for(int i=0; i<k; i++){
+        
+    }
+}
+
+int find_nmax_d(vector<int> vec, int n){
+    return 0; // NOTE: to finish
+}
+
 
 void solve(int t) {
     int N, K;
